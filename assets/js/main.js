a2fc46af955d72540fddd209ff14a5c9336875ea
Jahred Libres <peterbarnum39@gmail.com>
2025-09-06 01:11:54 +0800

    Update main.js - 9/6/2025

--- assets/js/main.js
+++ assets/js/main.js
@@ -311,6 +311,44 @@
         });
     });
   };
+
+  /**
+   * Software Bar - Dock
+   */
+
+  const icons = document.querySelectorAll(".ico");
+
+  const resetIcons = () => {
+    icons.forEach((item) => {
+      item.style.transform = "scale(1) translateY(0px)";
+    });
+  };
+
+  icons.forEach((item, index) => {
+    item.addEventListener("mouseover", () => focus(index));
+    item.addEventListener("mouseleave", resetIcons);
+  });
+
+  const focus = (index) => {
+    resetIcons();
+    const transformations = [
+      { idx: index - 2, scale: 1.1, translateY: 0 },
+      { idx: index - 1, scale: 1.2, translateY: -6 },
+      { idx: index, scale: 1.5, translateY: -10 },
+      { idx: index + 1, scale: 1.2, translateY: -6 },
+      { idx: index + 2, scale: 1.1, translateY: 0 }
+    ];
+
+    transformations.forEach(({ idx, scale, translateY }) => {
+      if (icons[idx]) {
+        console.log(scale);
+        icons[
+          idx
+        ].style.transform = `scale(${scale}) translateY(${translateY}px)`;
+      }
+    });
+  };
+
   
   /**
    * Navmenu Scrollspy
